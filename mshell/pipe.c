f035c1cd41303b0c82a2a9ae2176805b406bd631
Levrifon <rems-msn@hotmail.fr>
2014-11-27 11:11:55 +0100

    finished with n pipes

--- mshell/pipe.c
+++ mshell/pipe.c
@@ -16,39 +16,72 @@
 #include "cmd.h"
 
 void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
-	int fds[2];
+	int fds[MAXCMDS][2];
 	int pid;
+	int i = nbcmd -1, j;
 	char * bob;
-	assert(pipe(fds) != -1);
+	assert(pipe(fds[0]) != -1);
+	
 	switch(pid = fork()) {
-		case -1 : 
+		case -1:
+			printf("erreur, mon cerveau explose\n");
 			assert(0);
-		case 0	:
-			/*commande n*/
+		case 0 :
+			/*commande 0*/
 			setpgid(0,0);
-			dup2(fds[0],STDIN_FILENO);
-			close(fds[0]);
-			close(fds[1]);
-			execvp(cmds[nbcmd-1][0], cmds[nbcmd-1]);
+			/* on redirige la sortie vers la sortie standard */
+			dup2(fds[0][1],STDOUT_FILENO);
+			close(fds[0][1]);
+			close(fds[0][0]);
+			execvp(cmds[0][0],cmds[0]);
 			assert(0);
 	}
-	switch(fork()) {
-		case -1:
-			assert(0);
+	
+	for(i = 1; i<nbcmd- 1; i++){
+		assert(pipe(fds[i]) != -1);
+		
+		switch(fork()){
+			case -1:
+				printf("erreur, mon cerveau explose\n");
+				assert(0);
 		case 0 :
 			/*commande 0*/
 			setpgid(0,pid);
 			/* on redirige la sortie vers la sortie standard */
-			dup2(fds[1],STDOUT_FILENO);
-			close(fds[0]);
-			close(fds[1]);
-			execvp(cmds[0][0],cmds[0]);
+			dup2(fds[i][1],STDOUT_FILENO);
+			dup2(fds[i-1][0],STDIN_FILENO);
+			for(j=0; j<i; j++){
+				close(fds[j][0]);
+				close(fds[j][1]);
+			}
+			execvp(cmds[i][0],cmds[i]);
+			assert(0);
+		}
+	}
+	
+	switch(fork()){
+		case -1:
+			printf("erreur, mon cerveau explose\n");
+			assert(0);
+		case 0 :
+			/*commande n*/
+			setpgid(0,pid);
+			dup2(fds[i-1][0],STDIN_FILENO);
+			for(j=0; j<i; j++){
+				close(fds[j][0]);
+				close(fds[j][1]);
+			}
+			
+			execvp(cmds[i][0],cmds[i]);
 			assert(0);
 	}
+	
 	bob = strcat(cmds[0][0], "|"); 
 	bob = strcat(bob, cmds[1][0]);
-	close(fds[0]);
-	close(fds[1]);
+	for(j=0; j<i; j++){
+		close(fds[j][0]);
+		close(fds[j][1]);
+	}
 	if(bg == 1) {
 		jobs_addjob(pid,BG,bob); 
 	} else {
